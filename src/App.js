97b526ec38295e8304d286ca595fce970fdd55dc
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-07-13 15:01:19 -0300

    Component produto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Products from './components/Products';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Warrior Gamer Cart</a>
-      </header>
-      <main>
-        Lista de produtos
-      </main>
-      <footer>
-        Desenvolvido por Leonardo Dias.
-      </footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Warrior Gamer Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">
+              Carrinho
+            </div>
+          </div>
+        </main>
+        <footer>
+          Desenvolvido por Leonardo Dias.
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import formatCurrency from '../utils';
+
+export default class Products extends Component {
+  render() {
+    return (
+      <div className="test">
+        <ul className="products">
+          {this.props.products.map(product => (
+            <li key={product.id}>
+              <div className="product">
+                <a href={"#" + product._id}>
+                  <img src={product.image} alt={product.name}/>
+                  <p>
+                    {product.name}
+                  </p>
+                </a>
+                <div className="product-price">
+                  <div>
+                    {formatCurrency(product.price)}
+                  </div>
+                  <div className="btn">
+                      <img
+                        className="cart"
+                        src="/images/cart.svg"
+                        alt="Icone carrinho"
+                      />
+                      <span>Adicionar ao carrinho </span>{" "}
+                    </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
